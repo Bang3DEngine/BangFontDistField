681d8bef0fb6d2eb5ad88e2780f6f226f48b0a3d
sephirot47 <victorantondominguez@gmail.com>
2017-09-11 22:19:44 +0200

    Fixed stuff

--- Main.cpp
+++ Main.cpp
@@ -2,9 +2,9 @@
 
 #include "Bang/File.h"
 #include "Bang/Mesh.h"
+#include "Bang/Font.h"
 #include "Bang/Scene.h"
-#include "Bang/G_Font.h"
-#include "Bang/G_Shader.h"
+#include "Bang/Shader.h"
 #include "Bang/Resources.h"
 #include "Bang/Transform.h"
 #include "Bang/SystemUtils.h"
@@ -12,9 +12,11 @@
 #include "Bang/ImageEffects.h"
 #include "Bang/SceneManager.h"
 #include "Bang/ShaderProgram.h"
-#include "Bang/G_Framebuffer.h"
-#include "Bang/G_RenderTexture.h"
-#include "Bang/G_FontSheetCreator.h"
+#include "Bang/Framebuffer.h"
+#include "Bang/RenderTexture.h"
+#include "Bang/FontSheetCreator.h"
+
+using namespace Bang;
 
 void DoPass()
 {
@@ -30,30 +32,26 @@ int main(int argc, char **argv)
     app.CreateWindow();
 
     Path ttfPath = EPATH("Fonts/Ubuntu.ttf");
-    constexpr int loadCharSize = 512;
+    constexpr int loadCharSize = 256;
     constexpr int radius = loadCharSize / 2;
     constexpr int outputCharSize = 128;
     constexpr float signedOffset = 0.25f;
 
-    G_Font *font = new G_Font();
+    Font *font = new Font();
     font->SetLoadSize(loadCharSize);
     font->Import(ttfPath);
 
-    //*
     String chars = "";
     chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     chars += "abcdefghijklmnopqrstuvwxyz";
     chars += "0123456789.,-;:_^-+*[]{}()?!%&/\\=><#@ ";
-    /*/
-    String chars = "A";
-    //*/
 
     const GL::Attachment color0 = GL::Attachment::Color0;
     const GL::Attachment color1 = GL::Attachment::Color1;
     const Array<GL::Attachment> colorArr0 = {color0};
     const Array<GL::Attachment> colorArr1 = {color1};;
 
-    Array<G_Image> charDistFieldImages;
+    Array<Imageb> charDistFieldImages;
     Array<Vector2i> charSpreadOffsets;
     for (int i = 0; i < chars.Size(); ++i)
     {
@@ -61,8 +59,8 @@ int main(int argc, char **argv)
         Debug_Log("Processing char '" << c <<
                   "' (" << (i+1) << "/" << chars.Size() << ")");
 
-        G_Texture2D charBitmap;
-        if (!G_FontSheetCreator::LoadAtlasTexture(font->GetTTFFont(),
+        Texture2D charBitmap;
+        if (!FontSheetCreator::LoadAtlasTexture(font->GetTTFFont(),
                                                   &charBitmap, String(c),
                                                   nullptr, 0))
         {
@@ -71,23 +69,23 @@ int main(int argc, char **argv)
             return 1;
         }
 
-        G_ImageG<Byte> charBitmapImg = charBitmap.ToImage(false);
+        Imageb charBitmapImg = charBitmap.ToImage(false);
         Vector2i charSize = charBitmapImg.GetSize();
         charSize = Vector2i::Max(charSize, Vector2i::One);
-        charBitmapImg.Export( Path("Char_" + String(int(c)) + ".png") );
+        // charBitmapImg.Export( Path("Char_" + String(int(c)) + ".png") );
 
-        G_Image charImgMargined = charBitmapImg;
+        Imageb charImgMargined = charBitmapImg;
         charImgMargined.AddMargins(Vector2i(radius+1), Color::Zero,
                                    ImageAspectRatioMode::KeepExceeding);
-        charImgMargined.Export( Path("Char_" + String(int(c)) + "M.png") );
+        // charImgMargined.Export( Path("Char_" + String(int(c)) + "M.png") );
 
-        G_ImageG<Byte> charOutlineImg;
+        Imageb charOutlineImg;
         ImageEffects::Outline(charImgMargined, &charOutlineImg);
 
         Texture2D charOutline;
         charOutline.Import(charOutlineImg);
 
-        G_Framebuffer framebuffer(charImgMargined.GetWidth(),
+        Framebuffer framebuffer(charImgMargined.GetWidth(),
                                   charImgMargined.GetHeight());
         framebuffer.Bind();
         framebuffer.CreateColorAttachment(color0, GL::ColorFormat::RGBA_Float16);
@@ -96,7 +94,7 @@ int main(int argc, char **argv)
         framebuffer.ClearColor(Color::Zero);
 
         // First pass
-        G_ShaderProgram spFirstPass;
+        ShaderProgram spFirstPass;
         spFirstPass.Load( Path("pass.vert"), Path("firstPass.frag") );
         framebuffer.SetDrawBuffers(colorArr0);
         spFirstPass.Bind();
@@ -106,7 +104,7 @@ int main(int argc, char **argv)
         spFirstPass.UnBind();
 
         // Iterative passes, to increase distance boundary
-        G_ShaderProgram spPass;
+        ShaderProgram spPass;
         spPass.Load( Path("pass.vert"), Path("pass.frag") );
         spPass.Bind();
         spPass.Set("ImgSize", Vector2(charImgMargined.GetSize()));
@@ -121,9 +119,9 @@ int main(int argc, char **argv)
         }
         spPass.UnBind();
 
-        G_RenderTexture *charDistFieldTex = framebuffer.GetAttachmentTexture(
+        RenderTexture *charDistFieldTex = framebuffer.GetAttachmentTexture(
                     framebuffer.GetCurrentDrawAttachments().Front());
-        G_Imagef charDFImg = charDistFieldTex->ToImage<float>();
+        Imagef charDFImg = charDistFieldTex->ToImage<float>();
 
         Vector2i minPixel = charDFImg.GetSize() - Vector2i::One;
         Vector2i maxPixel = Vector2i::Zero;
@@ -163,17 +161,17 @@ int main(int argc, char **argv)
         Vector2i dfFittedSize = (maxPixel-minPixel);
         if (dfFittedSize.x > 0 && dfFittedSize.y > 0)
         {
-            G_Image charDFImgFitted(dfFittedSize.x, dfFittedSize.y);
+            Imageb charDFImgFitted(dfFittedSize.x, dfFittedSize.y);
             charDFImgFitted.Copy(charDFImg.To<Byte>(),
                                  Recti(minPixel, maxPixel),
                                  Recti(Vector2i::Zero, dfFittedSize));
 
-            charDFImgFitted.Export( Path("DistField_" + String(int(c)) + "_FITTED_0.png") );
-            G_Image charDFImgFittedSmall = charDFImgFitted;
+            // charDFImgFitted.Export( Path("DistField_" + String(int(c)) + "_FITTED_0.png") );
+            Imageb charDFImgFittedSmall = charDFImgFitted;
             charDFImgFittedSmall.Resize(Vector2i(outputCharSize),
                                         ImageResizeMode::Linear,
                                         ImageAspectRatioMode::Keep);
-            charDFImgFittedSmall.Export( Path("DistField_" + String(int(c)) + "_FITTED_1.png") );
+            // charDFImgFittedSmall.Export( Path("DistField_" + String(int(c)) + "_FITTED_1.png") );
             charDistFieldImages.PushBack( charDFImgFittedSmall );
 
             Vector2 charScalingDown(Vector2(charDFImgFittedSmall.GetSize()) /
@@ -184,19 +182,20 @@ int main(int argc, char **argv)
         }
         else
         {
-            G_Image empty;
+            Imageb empty;
             empty.Create(1, 1, Color::Zero);
             charDistFieldImages.PushBack(empty);
+            charSpreadOffsets.PushBack(Vector2i::Zero);
         }
 
         framebuffer.UnBind();
     }
 
     Array<Recti> imageOutputRects;
-    G_Image fontDistFieldImg =
-            G_FontSheetCreator::PackImages(charDistFieldImages, 5,
-                                           &imageOutputRects,
-                                           Color::White);
+    Imageb fontDistFieldImg =
+            FontSheetCreator::PackImages(charDistFieldImages, 5,
+                                         &imageOutputRects,
+                                         Color::White);
 
     String fileName = ttfPath.GetName() + "_DistField";
     fontDistFieldImg.Export( Path(fileName).AppendExtension("png") );
@@ -209,19 +208,16 @@ int main(int argc, char **argv)
         xmlInfo.Set("SpreadOffset_" + String(int(c)), spreadOffset);
 
         const Recti &charDFRect = imageOutputRects[i];
-        Recti actualCharRect = Recti(spreadOffset + charDFRect.GetMin(),
+        Recti actualCharRect = Recti(charDFRect.GetMin() + spreadOffset,
                                      charDFRect.GetMax() - spreadOffset);
         xmlInfo.Set("CharRect_" + String(int(c)), actualCharRect);
 
-        fontDistFieldImg.GetSubImage(charDFRect).Export(Path("NotActual_" + String(i) + ".png"));
-        fontDistFieldImg.GetSubImage(actualCharRect).Export(Path("Actual_" + String(i) + ".png"));
+        // fontDistFieldImg.GetSubImage(charDFRect).Export(Path("NotActual_" + String(i) + ".png"));
+        // fontDistFieldImg.GetSubImage(actualCharRect).Export(Path("Actual_" + String(i) + ".png"));
     }
     xmlInfo.Set("LoadSize", font->GetLoadSize());
     xmlInfo.Set("SignedOffset", signedOffset);
     File::Write(Path(fileName).AppendExtension("info"), xmlInfo.ToString());
-
-    // Put every character in the final atlas
-    // SystemUtils::System("xdg-open DistField_65.png");
 
     return 0;
 }
